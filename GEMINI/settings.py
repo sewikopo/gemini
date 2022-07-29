e4816bbc020cbdd680be6c89858859d6a2876913
Marc <sewikopo89b@gmail.com>
2022-07-29 14:39:27 +0700

    second commit

--- GEMINI/settings.py
+++ GEMINI/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from pickle import TRUE
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -148,3 +148,4 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+django_heroku.settings(locals())
